a671d4c7b81c4bfaf8c8bf598d09f63ef5da62fc
andrit <arnewmediatech@gmail.com>
2014-11-06 00:31:55 -0500

    adding some style

--- js/app.js
+++ js/app.js
@@ -3,16 +3,16 @@ $(document).ready(function(){
 		for (var i=1; i <= 100; i++)
 {
     if (i % 15 == 0){
-        $('<p>').text("FizzBuzz").appendTo('.count-off');
+        $('<li>').text("FizzBuzz").appendTo('.count-off');
     }
     else if (i % 3 == 0){
-        $('<p>').text("Fizz").appendTo('.count-off');
+        $('<li>').text("Fizz").appendTo('.count-off');
     }
     else if (i % 5 == 0){
-        $('<p>').text("Buzz").appendTo('.count-off');
+        $('<li>').text("Buzz").appendTo('.count-off');
     }
     else{
-        $('<p>').text(i).appendTo('.count-off');
+        $('<li>').text(i).appendTo('.count-off');
     }
 }
 	});
