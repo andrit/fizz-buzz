1d66f5f619dd22604260a2c46841342850122763
andrit <arnewmediatech@gmail.com>
2014-11-06 00:43:39 -0500

    adding more styles via jquery

--- js/app.js
+++ js/app.js
@@ -3,13 +3,13 @@ $(document).ready(function(){
 		for (var i=1; i <= 100; i++)
 {
     if (i % 15 == 0){
-        $('<li>').text("FizzBuzz").appendTo('.count-off');
+        $('<li>').text("FizzBuzz").appendTo('.count-off').addClass('fizzbuzz');
     }
     else if (i % 3 == 0){
-        $('<li>').text("Fizz").appendTo('.count-off');
+        $('<li>').text("Fizz").appendTo('.count-off').addClass('fizz');
     }
     else if (i % 5 == 0){
-        $('<li>').text("Buzz").appendTo('.count-off');
+        $('<li>').text("Buzz").appendTo('.count-off').addClass('buzz');
     }
     else{
         $('<li>').text(i).appendTo('.count-off');
